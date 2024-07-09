5d5f08e227c4e9b015b1a1a8e4bf1d87b6b6b9cd
breadsroofses <yaroslavhorovyi@gmail.com>
2024-07-09 17:51:00 +0300

    gooo

--- bebeb.py
+++ bebeb.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import*
+from PyQt5.QtWidgets import *
 import random
 
 app = QApplication([])
@@ -6,23 +6,20 @@ app = QApplication([])
 window = QWidget()
 
 winner_lbl = QLabel("Переможець: ")
-generate_winner_btn = QPushButton ("Отримати переможця")
+generate_winner_btn = QPushButton("Отримати переможця")
 
 main_line = QVBoxLayout()
 main_line.addWidget(winner_lbl)
 main_line.addWidget(generate_winner_btn)
 
 
-
 def winner_func():
     num = random.randint(1, 4)
     winner_lbl.setText("Переможець: " + str(num))
 
+
 generate_winner_btn.clicked.connect(winner_func)
-
-
 
 window.setLayout(main_line)
 window.show()
 app.exec()
-
